d12e42242c3ee749e6ced4feddf8519bc4278657
leitdeux <elliott.fiedler@gmail.com>
2020-09-14 21:02:21 +0900

    Implement basic ball drawing routines

--- main.asm
+++ main.asm
@@ -20,40 +20,7 @@
 ;
 ; Stretch goals:
 ; - title screen
-; - increase velocity as scores increase
-;===========================================================================
-
-;===========================================================================
-; Declare variables starting from memory address $80
-;===========================================================================
-    seg.u Variables
-    org $80
-
-;-----------------------------------------------------------------------
-; Variables
-;-----------------------------------------------------------------------
-ScanlineCount .byte         ; number of visible scanlines
-Player0Y .byte              ; P0 y-position
-Player1Y .byte              ; P1 y-position
-BallX .byte                 ; ball x-position
-BallY .byte                 ; ball y-position
-
-;-----------------------------------------------------------------------
-; Constants
-;-----------------------------------------------------------------------
-SCANLINE_COUNT = 96         ; 2-line kernel, so 192 / 2 = 96
-PLAYER_HEIGHT = 16
-PLAYER_Y_INIT = 47
-PLAYER_SPRITE = %00000111
-
-BALL_HEIGHT = 6
-BALL_SIZE = %00101000       ; 3 pixels
-
-COLOR_WHITE = $0e
-COLOR_BLUE = $a0
-COLOR_PURPLE = $8e
-COLOR_YELLOW = $1a
-
+; - increase ball speed as scores increase
 
 ; TODO:
 ; 1. -- draw players
@@ -67,8 +34,47 @@ COLOR_YELLOW = $1a
 ; DONE - p1 down, up
 ; DONE - generalize player input
 ; 3. -- draw ball
+; - set x-position (fine)
+; - draw at y-position
 ; 4. -- move ball
-; 5. -- handle collisions
+; 5. -- handle ball collisions with top, bottom, left, right
+; 6. -- handle collisions between p0,p1 and ball
+;===========================================================================
+
+;===========================================================================
+; Declare variables starting from memory address $80
+;===========================================================================
+    seg.u Variables
+    org $80
+
+;-----------------------------------------------------------------------
+; Variables
+;-----------------------------------------------------------------------
+ScanlineCount .byte         ; number of visible scanlines
+
+Player0Y .byte              ; P0 y-position
+Player1Y .byte              ; P1 y-position
+
+BallX .byte                 ; ball x-position
+BallY .byte                 ; ball y-position
+
+;-----------------------------------------------------------------------
+; Constants
+;-----------------------------------------------------------------------
+SCANLINE_COUNT = 96         ; 2-line kernel; 192 / 2 = 96
+
+PLAYER_HEIGHT = 16
+PLAYER_Y_INIT = 47          ; the initial y-position in pixels
+PLAYER_SPRITE = %00000111
+
+BALL_HEIGHT = 5
+BALL_X_INIT = 32            ; the initial x-position in pixels
+BALL_SIZE = %00101000       ; 3 pixels
+
+COLOR_WHITE = $0e
+COLOR_BLUE = $a0
+COLOR_PURPLE = $8e
+COLOR_YELLOW = $1a
 
 ;===========================================================================
 ; Init code segment
@@ -87,13 +93,15 @@ Start:
     sta Player1Y
     sta BallY
 
-    lda #COLOR_PURPLE        ; set player colors
+    lda #BALL_X_INIT        ; set default x-position
+    sta BallX
+
+    lda #COLOR_PURPLE       ; set player colors
     sta COLUP0
     lda #COLOR_YELLOW
     sta COLUP1
 
 NextFrame:
-
     lsr SWCHB               ; handle game reset
     bcc Start
 
@@ -123,22 +131,39 @@ OutputVBlank:
     lda #0                  ; disable VBLANK
     sta VBLANK
 
+    sta HMCLR               ; reset previous fine x-position(s)
+
+; TODO: -- refactor into a general subroutine for setting x of any object
+SetBallXPosition:
+    lda BallX
+    sec                     ; set carry flag before subtract
+.Divide15Loop:
+    sbc #15                 ; subtract 15 (TIA clocks) from A
+    bcs .Divide15Loop       ; loop until carry flag no longer set
+    eor #7                  ; adjust remainder in A between -8 and 7 using XOR
+    asl                     ; bit-shift left 4 times, as HMBL uses left-most 4 bits
+    asl
+    asl
+    asl
+    sta HMBL                ; set ball fine x offset
+    sta RESBL               ; set ball's coarse x offset
+
 ;===========================================================================
 ; Visible scanlines
 ;===========================================================================
 DrawGame:
-    SLEEP 12                ; set P0 x-position
+    ; Refactor this: -- we need to use fine positioning instead of sleep!
+    SLEEP 16                ; set P0 x-position
     sta RESP0
 
-    ; FIXME -- this only works with a stationary ballww
-    SLEEP 20                ; set ball x-position
-    sta RESBL
-
-    SLEEP 17                ; set P1 x-position
+    SLEEP 32                ; set P1 x-position
     sta RESP1
 
     lda #COLOR_BLUE         ; set background color
     sta COLUBK
+
+    lda #1                  ; delay drawing of P0 until P1 is drawn
+    sta VDELP0
 
     lda ScanlineCount       ; load visible scanline count
 VisibleScanlines:
@@ -146,16 +171,29 @@ VisibleScanlines:
     jsr HandlePlayerDraw    ; 6
     sta WSYNC               ; scanline (1 of 2) we sync after because we want
                             ; P0 and P1 to be drawn on the same line
+
     lda ScanlineCount       ; restore scanline value for next player draw
     ldx #1                  ; draw P1
     jsr HandlePlayerDraw    ; 6
-    sta WSYNC               ; scanline (2 of 2)
 
-    ; TODO -- handle ball drawing here
-    ;lda #%00101000         ; set ball width to 3 pixels
-    ;sta CTRLPF
-    ;lda #2                  ; draw ball graphics
-    ;sta ENABL
+    lda ScanlineCount       ; load current scanline
+HandleBallDraw:
+    ldx #BALL_SIZE          ; load ball width in X
+    ldy %00000010           ; load ball graphics enable value in Y
+    sec                     ; set carry flag before subtract
+    sbc BallY               ; subtract ball y-position from current scanline count
+    cmp #BALL_HEIGHT        ; is ball found in scanline?
+    bcc .DrawBall
+    ldx #0                  ; draw nothing
+    ldy #0
+.DrawBall:
+    txa                     ; transfer ball width to A
+    sta CTRLPF
+    tya                     ; transfer graphics enable value to A
+    sta ENABL               ; enable ball graphic
+    sta WSYNC               ; draw scanline (2 of 2)
+
+    ;sta HMOVE              ; where does HMOVE get called??
 
     dec ScanlineCount       ; decrement visible scanline count
     lda ScanlineCount       ; load updated scanline count
@@ -204,13 +242,12 @@ HandlePlayerDraw subroutine
     ldy #PLAYER_SPRITE      ; load player sprite in Y
     sec                     ; set carry for subtract
     sbc Player0Y,x          ; subtract the player-y from A
-    cmp #PLAYER_HEIGHT      ; in sprite?
+    cmp #PLAYER_HEIGHT      ; is sprite found in scanline?
     bcc .DrawPlayer
     ldy #0                  ; reset value in Y (don't draw player)
 .DrawPlayer:
     clc                     ; clear carry flag
     tya                     ; transfer sprite value in Y to A
-    ;sta WSYNC               ; scanline sync (1 of 2)
     sta GRP0,x              ; store player-0 bitmap
     rts                     ; 6
 
