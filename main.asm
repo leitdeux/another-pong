49a7057a5496562726db31b9bafa9402d4ddf962
leitdeux <elliott.fiedler@gmail.com>
2020-09-19 08:56:47 +0900

    Implement screen top and bottom collision handling for Ball

--- main.asm
+++ main.asm
@@ -5,7 +5,7 @@
 ;===========================================================================
     .include "include/vcs.h"
     .include "include/macro.h"
-
+    .include "include/timer.h"
 ;===========================================================================
 ; Pong
 ;
@@ -40,8 +40,11 @@
 ; DONE - create general subroutine for settings x-pos offset
 ; DONE - use it directly after vblank
 ; 4. -- move ball
-; - velocity
+; DONE - horizontal, vertical
+; DONE - velocity
 ; 5. -- handle ball collisions with top, bottom, left, right
+; DONE - top
+; DONE - bottom
 ; 6. -- handle collisions between p0,p1 and ball
 ;===========================================================================
 
@@ -63,20 +66,29 @@ Player1Y .byte              ; P1 y-position
 
 BallX .byte                 ; ball x-position
 BallY .byte                 ; ball y-position
+BallYVel .byte              ; ball y velocity (signed)
+BallXVel .byte              ; ball x velocity (signed)
+BallXFrac .byte             ; ball x fractional value
+                            ; BallXVel is added to BallXFrac so that the Ball
+                            ; can move in incremenets less than 1px per frame
 
 ;-----------------------------------------------------------------------
 ; Constants
 ;-----------------------------------------------------------------------
 SCANLINE_COUNT = 96         ; 2-line kernel; 192 / 2 = 96
+SCREEN_Y_MAX = 94           ; top of the screen
+SCREEN_Y_MIN = 4            ; bottom of the screen
 
 PLAYER_HEIGHT = 16
-PLAYER_0_X_INIT = 0         ; initial P0 x-position
-PLAYER_1_X_INIT = 120       ; initial P1 x-position
-PLAYER_Y_INIT = 47          ; the initial y-position in pixels
+PLAYER_0_X_INIT = 0         ; initial P0 x-position (in pixels)
+PLAYER_1_X_INIT = 160       ; initial P1 x-position
+PLAYER_Y_INIT = 45          ; initial Player y-position
+PLAYER_Y_MAX = 80           ; max player y-position
 PLAYER_SPRITE = %00000111
 
-BALL_HEIGHT = 5
-BALL_X_INIT = 0             ; the initial x-position in pixels
+BALL_HEIGHT = 4
+BALL_X_INIT = 88            ; initial Ball x-position
+BALL_Y_INIT = 50            ; initial Ball y-position
 BALL_SIZE = %00101000       ; 3 pixels
 
 COLOR_WHITE = $0e
@@ -99,7 +111,14 @@ Start:
     lda #PLAYER_Y_INIT      ; 2, set default y-position
     sta Player0Y            ; 3
     sta Player1Y            ; 3
+
+    lda #BALL_Y_INIT        ; 2, set default ball y-position
     sta BallY               ; 3
+
+    lda #1                  ; 2, set default ball velocity
+    sta BallYVel            ; 3
+    lda #48                 ; 2
+    sta BallXVel
 
     lda #PLAYER_0_X_INIT    ; 2, set player default x-position
     sta Player0X            ; 3
@@ -205,7 +224,6 @@ HandleBallDraw:
     lda ScanlineCount       ; 3, load updated scanline count
     bne VisibleScanlines    ; 2, branch if all scanlines have been drawn
 
-    ;inc BallX
 ;===========================================================================
 ; Output overscan
 ;===========================================================================
@@ -216,11 +234,83 @@ HandleBallDraw:
     ; to ensure we spend only 29 scanlines worth of work in here
     ; at the end of it all, we call again WSYNC and HMOVE to apply horizontal motion updates
 
-    ldx #29                 ; 2
+    ;ldx #29                 ; 2
 OutputOverscan:
+    ;sta WSYNC               ; 3
+    ;dex                     ; 2
+    ;bne OutputOverscan      ; 2
+
+    TIMER_SETUP 29           ; use timer instead of iterating over each scanline
+
+;===========================================================================
+; Update x offsets for motion objects
+;===========================================================================
+    ; sta WSYNC               ; 3, clear horiz. motion registers
+    ; sta HMCLR               ; 3
+
+    ; lda BallX               ; 3, handle ball x-position
+    ; ldx #4                  ; 2
+    ; jsr HandleObjXPosition  ; 39
+
+    ; sta WSYNC               ; 3, sync scanline before setting horizontal registers
+    ; sta HMOVE               ; 3, apply *all* horizontal motion registers (HM__)
+
+;===========================================================================
+; Handle Ball collisions
+;===========================================================================
+    ldx BallY               ; load ball y-position in X
+    lda #$ff                ; 2, load -1 in A
+    cpx #SCREEN_Y_MAX       ; is ball at top of screen?
+    bcs .UpdateBallYVel
+    lda #1                  ; 2, load +1 in A
+    cpx #SCREEN_Y_MIN       ; is ball at bottom of screen?
+    bcc .UpdateBallYVel
+    lda BallYVel            ; load current y-velocity value
+.UpdateBallYVel:
+    sta BallYVel            ; set ball y-velocity to either -1, 1 or its current value
+
+;===========================================================================
+; Update Ball position via velocity
+;===========================================================================
+UpdateBallPosition:
+    sta WSYNC               ; 3, necessary?
+    sta HMCLR               ; 3
+
+    lda BallY               ; 3, update y-position
+    clc                     ; 2, clear carry
+
+; TODO - handle y-position update
+;     lda BallYVel
+;     bmi .DecrementBallYVel
+;     lda BallY
+;     adc BallYVel
+;     adc BallYVel
+; .DecrementBallYVel:
+;     lda BallY
+;     sec
+;     sbc BallYVel
+
+    adc BallYVel            ; 3, increment y-position by y-velocity
+    sta BallY               ; 3
+
+    lda BallXVel            ; 3, update x-position
+    bmi .MoveBallLeft       ; 2, if x-velocity < 0, move Ball left
+    clc                     ; 2, clear carry
+    adc BallXFrac           ; 3, increment x-position by x-velocity
+    sta BallXFrac           ; 3, BallXFrac += BallXVel
+    lda #$f0                ; 2
+    sta HMBL                ; 3, set ball motion register
+    bne .Foo                ; 2
+.MoveBallLeft:
+    sec                     ; 2, set carry flag
+    sbc BallXFrac           ; 3?, subtract x-velocity in A from BallXFrac
+    sta BallXFrac           ; 3, update x-velocity value
+    bcs .Foo                ; 2, branch if carry flag was set
+    lda #$10                ; 2
+    sta HMBL                ; 3, set ball motion register
+.Foo:
     sta WSYNC               ; 3
-    dex                     ; 2
-    bne OutputOverscan      ; 2
+    sta HMOVE               ; 3
 
 ;===========================================================================
 ; Handle player input for next frame (88)
@@ -236,19 +326,11 @@ HandlePlayerInputs:
     jsr HandlePlayerInput   ; 36
     stx Player1Y            ; 3, update P1 y-position
 
-;===========================================================================
-; Update x offsets for motion objects
-;
-;===========================================================================
-    sta WSYNC               ; 3, clear horiz. motion registers
-    sta HMCLR               ; 3
-
-    lda BallX               ; 3, handle ball x-position
-    ldx #4                  ; 2
-    jsr HandleObjXPosition  ; 39
-
-    sta WSYNC               ; 3, sync scanline before setting horizontal registers
-    sta HMOVE               ; 3, apply *all* horizontal motion registers (HM__)
+;-----------------------------------------------------------------------
+; Overscan timer expires
+;-----------------------------------------------------------------------
+    TIMER_WAIT              ; wait until 29 scanlines, and then strobe
+                            ; one more scanline for 30 total in overscan
 
 ;===========================================================================
 ; Draw next frame
@@ -285,14 +367,14 @@ HandlePlayerDraw subroutine
 HandlePlayerInput subroutine
     bit SWCHA               ; 3
     bne .HandleDownInput    ; 2
-    cpx #80                 ; 2, is y-position at top of screen?
+    cpx #PLAYER_Y_MAX       ; 2, is player y-position at top of screen?
     beq .HandleDownInput    ; 2
     inx                     ; 2, increment y-position in X
 .HandleDownInput:
     asl                     ; 2, bit-shift left to test the down input
     bit SWCHA               ; 3
     bne .HandleNoInput      ; 2
-    cpx #4                  ; 2, is y-position at bottom of screen?
+    cpx #SCREEN_Y_MIN       ; 2, is y-position at bottom of screen?
     beq .HandleNoInput      ; 2
     dex                     ; 2, decrement y-position in X
 .HandleNoInput:
@@ -316,7 +398,7 @@ HandleObjXPosition subroutine
     asl                     ; 2
     sta RESP0,x             ; 4, set coarse x-position for object x
     sta HMP0,x              ; 4, set fine x-position for object x
-    ; IDEA: -- do an additional: (?)
+    ; IDEA: -- do an additional: (so each object uses 2 scanlines instead of just one, might be useful)
     ; sta WSYNC
     ; sta HMOVE
     ; sta HMCLR
